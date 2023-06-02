de5951460fe683c7435574ceb9da8a224baf777f
“karandepale” <karandepale111@gmail.com>
2023-06-02 11:03:52 +0530

    Add:Created A Stack Ability to create a Stack of 56->30->70

--- StackQueueDay14/Program.cs
+++ StackQueueDay14/Program.cs
@@ -1,16 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StackQueueDay14
 {
+    public class Stack
+    {
+        private Node top; 
+
+        private class Node
+        {
+            public int Data;
+            public Node Next;
+        }
+
+        public void Push(int data)
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+
+            if (top == null)
+                top = newNode;
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+        }
+
+        public int Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return -1; 
+            }
+
+            int data = top.Data;
+            top = top.Next;
+            return data;
+        }
+
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+
+            Node current = top;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Stack Queue Implementation ...");
+
+            Stack stack = new Stack();
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+
+            stack.Display(); // Output: 70 30 56
         }
     }
 }
